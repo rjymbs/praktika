6e43a8b4cb0cd82791f14cdd3ac0d457ae24b9a9
Н <@HOME-PC>
2022-06-02 06:18:13 +0300

    Формирование файлов result и analysis, вычисление времени сортировки

--- praktika/praktika.cpp
+++ praktika/praktika.cpp
@@ -58,7 +58,64 @@ string* BubbleSort(string* wordArr) {
     return wordArr;
 }
 
+//формирование файлов вывода result и analysis
+void CreateOutput(string* textArr, string first_text) {
+    string text = "", word, s;
+    string alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"; // все буквы алфавита для проверки кириллицы 
+    int* array_count = new int[alphabet.size()]; //массив с информацией о количестве каждого слова на каждую букву из кириллицы
 
+    //Считаем количество слов на каждую букву алфавита и формируем слова начиная с "а" в алфавитном порядке
+    for (int i = 0; i < alphabet.size(); i++) {
+        int count = 0; 
+        for (int j = 0; j < amountOfWords; j++) {
+            word = textArr[j];
+            s = tolower(word[0]);
+            if (s[0] == alphabet[i]) {    // если 1 элемент слова совпадает с эл. из alphabet
+                if (count == 0)         // если 1 эл строки еще не нашелся в alphabet
+                    text += word;         // добавляем слово
+                else
+                    text += " " + word;     // если уже было, добавляем через пробел
+                count++;    //подсчет кол-во слов в тексте на каждую букву alphabet
+            }
+        }
+        if (count > 0)         //если счетчик слов больше 0
+            text += "\n";      //добавляем в text перенос на след. строку, где будут следующие на слова на другой элемент
+        array_count[i] = count;   // записываем кол-во слов на каждую букву алфавита
+    }
+
+    // вычисляем время сортировки
+    unsigned int time_start = clock(); // время до запуска сортировки
+    BubbleSort(textArr); // вызов сортировки
+    unsigned int time_end = clock(); // время после сортировки
+    Time = (time_end - time_start) / 1000.0; // нахождение времени 
+
+
+    //формируем файл result
+    ofstream targetFile("11.txt");
+    targetFile << text;     // переписываем text в файл
+    targetFile.close();     // закрываем файл
+
+    //формируем файл analysis
+    text = "Введенный текст:\n" + first_text + "\n\nВариант 13: кириллица, по алфавиту, по возрастанию, игнорировать числа, сортировка пузырьком\n";
+    text += "Количество слов: " + to_string(amountOfWords) + "\n";
+    stringstream stream;
+    stream << Time;   
+    text += "Время сортировки: " + stream.str() + " сек\n";
+    text += "Статистика (количество слов на каждую букву алфавита):\n";
+
+    //формирование количество слов по буквам алфавита
+    for (int i = 0; i < alphabet.size(); i++) {
+        text += alphabet[i];       // буква алфавита
+        text += " - ";         
+        text += to_string(array_count[i]);      // количество
+        if (i < alphabet.size() - 1)
+            text += "\n";          //переход к следующей букве на новую строку
+    }
+    
+    targetFile.open("12.txt"); 
+    targetFile << text; 
+    targetFile.close();
+}
 
 void start() {
     cout << "Введите название файла/путь к файлу: ";
@@ -79,6 +136,7 @@ void start() {
         
         string* words = ScanForWords(initial_text);//формируем слова в массив
         words = BubbleSort(words); //сортировка
+        CreateOutput(words, initial_text);//формирование файла result и analysis
     }
 }
 
